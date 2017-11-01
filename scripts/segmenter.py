f095388bb19453538693a9237b03174980a2504c
Francis Tyers <ftyers@prompsit.com>
2017-11-01 15:39:27 +0100

    segmenter

--- scripts/segmenter.py
+++ scripts/segmenter.py
@@ -2,7 +2,29 @@
 
 import sys 
 
+title = ''
 
-for line in sys.stdin.readlines(): 
-	print(line)
-
+for blokk in sys.stdin.read().split('\n\n'): 
+	paragraphs = blokk.split('\n')
+	if len(paragraphs) < 2: 
+		continue
+	title = paragraphs[0].strip().replace(' ', '_')
+	for line in paragraphs[1:]:
+		# Deal with abbreviations 
+		sentences = line.replace('. ', '.\n').split('\n')
+		sentIdx = 1
+		for sent in sentences:
+			sent = sent.strip()
+			if sent == '':
+				continue
+			print('# sent_id = %s%d' % (title, sentIdx));
+			print('# text = %s' % (sent))
+			# Deal with abbreviations and punctuation
+			tokens = sent.split(' ')
+			tokIdx = 1
+			for token in tokens:
+				print('%d\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s' % (tokIdx, token, '_','_','_','_','_','_','_','_'))
+				tokIdx += 1
+			print()
+			sentIdx += 1
+	
